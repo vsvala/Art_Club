b09254e98bf77fc8b0c21daa6035b149f9a1aec2
vsvala <virva.svala@gmail.com>
2026-07-06 22:26:56 +0300

    ci(sentry): add release commits and sourcemap upload on master pushes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,27 @@
-import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
-import { connect } from "react-redux";
-import { updateLoggedUser } from "../reducers/actionCreators/loginActions";
-import logo from "../images/tripleblue.svg";
+import React, { useEffect } from 'react'
+import * as Sentry from '@sentry/react'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { updateLoggedUser } from '../reducers/actionCreators/loginActions'
+import logo from '../images/tripleblue.svg'
 
 export const Home = (props) => {
   useEffect(() => {
-    updateLoggedUser();
-  }, []);
+    updateLoggedUser()
+  }, [])
+
+  const sendSentryTestError = () => {
+    Sentry.captureException(new Error('Sentry frontend test error'))
+  }
 
   return (
     <div>
-      {props.loggedUser && props.loggedUser.role === "nonMember" ? (
+      {props.loggedUser && props.loggedUser.role === 'nonMember' ? (
         <div className="nonMember">
           <h2>Art club</h2>
           <h3>Your membership application will be processed soon... </h3>
           <p>
-            {" "}
+            {' '}
             Once your application is approved, you`&apos;`ll get notification by
             email.
           </p>
@@ -25,11 +30,7 @@ export const Home = (props) => {
         <div className="home">
           <h1>Welcome To Art Club!</h1>
           <br />
-          <img
-            src={logo}
-            className="frontLogo"
-            alt="Art club LOGO"
-          />
+          <img src={logo} className="frontLogo" alt="Art club LOGO" />
           <br />
           <br />
           <p>
@@ -37,29 +38,39 @@ export const Home = (props) => {
             page where it is possible to upload max 10 pictures and short
             introduction about yourself. Added pictures will be displayed on
             main gallery page. You will also find Art club`&apos;`s upcoming
-            events and subscribe to our email-list.{" "}
+            events and subscribe to our email-list.{' '}
           </p>
           <p>
-            {" "}
+            {' '}
             <Link to="/login" className="login">
               Login
-            </Link>{" "}
-            or{" "}
+            </Link>{' '}
+            or{' '}
             <Link to="/register" className="register">
               Register
-            </Link>{" "}
+            </Link>{' '}
             to apply membership
           </p>
+          {process.env.NODE_ENV === 'development' ? (
+            <button
+              type="button"
+              className="button"
+              style={{ marginTop: '12px' }}
+              onClick={sendSentryTestError}
+            >
+              Send Sentry test error
+            </button>
+          ) : null}
         </div>
       )}
     </div>
-  );
-};
+  )
+}
 
 const mapStateToProps = (state) => {
   return {
     loggedUser: state.loggedUser.loggedUser,
-  };
-};
+  }
+}
 
-export default connect(mapStateToProps, { updateLoggedUser })(Home);
+export default connect(mapStateToProps, { updateLoggedUser })(Home)
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import * as Sentry from '@sentry/react'
 import { Provider } from 'react-redux'
 import store from './reducers/store'
 import App from './App'
@@ -7,9 +8,25 @@ import './index.css'
 import { createRoot } from 'react-dom/client'
 
 const root = createRoot(document.getElementById('root'))
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  environment: process.env.NODE_ENV || 'development',
+  release: process.env.REACT_APP_SENTRY_RELEASE,
+  tracesSampleRate: 0.1,
+  dataCollection: {
+    // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/react/configuration/options/#dataCollection
+    // userInfo: false,
+    // httpBodies: []
+  },
+})
+
 root.render(
   <Provider store={store}>
-    <App />
+    <Sentry.ErrorBoundary fallback={<p>Something went wrong.</p>}>
+      <App />
+    </Sentry.ErrorBoundary>
   </Provider>,
 )
 //App is a child of Provider to make connect to work and store is Providers attribute
